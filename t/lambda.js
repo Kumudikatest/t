e9035b9624c79d87be93e61cad81ec2f646ef8ce
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-03-13 11:48:29 +0530

    retest 537

--- t/lambda.js
+++ t/lambda.js
@@ -1,6 +1,4 @@
-let AWS = require('aws-sdk');
-
-exports.handler = function (event, context, callback) {
-//
-    callback(null, { "message": "Successfully executed" });
+exports.handler = function(event, context, callback) {
+    
+    callback(null, {"message": "Successfully executed"});
 }
