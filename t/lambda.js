f9be01c5ea60c8c426a1e646afb4cf209bcd8e16
Kumudikatest <kumudikarupasinghe+adrt@gmail.com>
2019-06-07 18:10:44 +0530

    test

--- t/lambda.js
+++ t/lambda.js
@@ -1,4 +1,26 @@
-exports.handler = function(event, context, callback) {
-    
-    callback(null, {"message": "Successfully executed"});
+let google = require('googleapis').google;
+let _auth = require('./Authorizer');
+const datastore = google.datastore('v1');
+
+exports.handler = function (event, context, callback) {
+    datastore.projects.beginTransaction({
+        projectId: process.env.GCP_PROJECT,
+        resource: {
+            transactionOptions: {
+                readWrite: {}
+            }
+        }
+    }).then(response => {
+        console.log(response.data);           // successful response
+        /*
+        response.data = {
+            "transaction": "<transaction ID>"
+        }
+        */
+    })
+        .catch(err => {
+            console.log(err, err.stack); // an error occurred
+        });
+
+    callback(null, { "message": "Successfully executed" });
 }
